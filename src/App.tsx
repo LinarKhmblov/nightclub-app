132b74775a3d43c210eb8799fd253e540d17e059
maratkhambelov <maratkhambelov@gmail.com>
2022-01-09 14:48:28 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactElement, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPersons, fetchTracks, IRootState } from './store/store';
+import { ITrack } from './store/musicReducer';
+import { IPerson } from './store/personsReducer';
+import { Club } from './components/Club';
+import { DisplayDataCurrentTrack, DisplayDataSeparatedPersons } from './components/DisplayDataComponents';
+import { queue } from './QueueMusic';
+import './App.scss';
+import './components/ComponentsStyles.scss';
 
-function App() {
+export interface ISeparatedPersons {
+  drinkingPersons: IPerson[];
+  dancingPersons: IPerson[];
+}
+
+export const App = (): ReactElement => {
+  const dispatch = useDispatch();
+  const persons = useSelector<IRootState, IPerson[]>((state) => state.persons.persons);
+  const currentTrack = useSelector<IRootState, ITrack>((state) => state.music.currentTrack);
+  const tracks = useSelector<IRootState, ITrack[]>(state => state.music.tracks);
+  const [separatedPersons, setSeparatedPersons] = useState<{drinkingPersons: IPerson[], dancingPersons: IPerson[]}>({ drinkingPersons: [], dancingPersons: [] });
+
+  useEffect(() => {
+    // init data
+    dispatch(fetchTracks());
+    dispatch(fetchPersons());
+    //FIXME: wrong empty deps
+  }, []);
+
+  useEffect(() => {
+    // run queue of tracks
+    queue.init(tracks);
+  }, [tracks]);
+
+  useEffect(() => {
+    // if track or persons changed -> divide persons
+    setSeparatedPersons(
+      persons.reduce((separatedPersons: ISeparatedPersons, person) => {
+        person.skills.includes(currentTrack.genre) ? separatedPersons.dancingPersons.push(person) : separatedPersons.drinkingPersons.push(person);
+        return separatedPersons;
+      }, { dancingPersons: [], drinkingPersons: []}),
+    );
+  }, [currentTrack, persons]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayDataCurrentTrack {...currentTrack}/>
+      <Club {...separatedPersons} />
+      <DisplayDataSeparatedPersons {...separatedPersons}/>
     </div>
   );
 }
-
-export default App;
